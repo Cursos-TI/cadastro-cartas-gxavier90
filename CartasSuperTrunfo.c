555c97a59568199dca777c7d17cc0836dd193d26
Gabriel Xaiver <202510051781@alunos.estacio.br>
2025-11-26 13:58:01 +0000

    encontrei um erro na exibição código da carta 01 aparece AB

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,7 +5,7 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+  // Área para definição das variáveis para armazenar as propriedades das cidades 
 
     // variáveis para o cadastro da carta 1 do Desafio Super Trunfo
 
