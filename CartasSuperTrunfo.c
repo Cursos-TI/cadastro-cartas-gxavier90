70eccb3574684041bfcbc1791f019172e64b097c
Gabriel Xaiver <202510051781@alunos.estacio.br>
2025-12-01 12:05:35 +0000

    printf e scanf da primeira variavel - sem erros

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -29,9 +29,13 @@ int main() {
 
   // Área para entrada de dados
 
-  printf ("\nDESAFIO SUPER TRUNFO\n\n");
+  printf ("\nDESAFIO SUPER TRUNFO\n");
 
   // Aqui inicia-se o cadastro das cartas 01 e 02 para o Desafio Super Trunfo
+
+  printf("CARTA 1\n"); // CADASTRO DA CARTA 01
+  printf("Digite o código do estado: \n");
+  scanf("%c", &estado_1);
 
 
 
